4b1b68108f44799a2bc6cd4f656b9556dc4f5e00
bushyur <yuriy.bush@db.com>
2015-09-18 00:00:33 +0100

    Initial revision. non-compilable, no unit tests

---
+++ ServiceScheduler/ConfigProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ServiceScheduler
+{
+	public class ConfigProvider
+	{
+		protected IList<DateTime> _executionTimes;
+
+		public ConfigProvider ()
+		{
+		}
+
+		public void Setup()
+		{
+			Pause ();
+			_executionTimes.Clean ();
+			var recurrentTimes = _dataProvider.GetRecurrentTimes ();
+			AddTodayTimes (recurrentTimes);
+			AddTomorrowTimes (recurrentTimes);
+			AddOnceTimes (_dataProvider.GetOnceTimes ());
+			Resume ();
+		}
+
+		protected void AddTodayTimes(IList<string> recurrentTimeCollection)
+		{
+		}
+
+		protected void AddTomorrowTimes(IList<string> recurrentTimeCollection)
+		{
+		}
+
+		protected void AddOnceTimes(IList<string> timeCollection)
+		{
+			
+		}
+
+		public DateTime GetNextExecutionTime()
+		{
+			//TODO: wait if Pause
+			//TODO: find nearest execution time in the collection
+		}
+
+		public Interval GetMinimalTimerInterval()
+		{
+			//TODO: wait if Pause
+
+		}
+
+		public Interval GetMinimalSleepInterval()
+		{
+			//TODO: wait if Pause
+			//TODO: return minimal of (NextExecution - Now) and DefaultSleepInterval
+		}
+	}
+}
+
---
+++ ServiceScheduler/ServiceCaller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ServiceScheduler
+{
+	public class ServiceCaller
+	{
+		protected Action _doWork;
+		protected Timer _timer;
+		protected IConfigProvider _configProvider;
+
+		public ServiceCaller (IConfigProvider configProvider)
+		{
+			_configProvider = configProvider;
+		}
+
+		public void MainLoop(Action doWork)
+		{
+			_doWork = doWork;
+			while(true)
+			{
+				if ((_configProvider.GetNextExecutionTime () - DateTime.Now) < _configProvider.GetMinimalTimerInterval ()) 
+				{
+					_doWork ();//TODO:background thread?
+				}
+				else
+				{
+					_timer.Sleep (_configProvider.GetMinimalSleepInterval ());
+				}
+			}
+		}
+	}
+}
+
