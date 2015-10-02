efff0b7047ef47a344bae4c91a62cb046f880638
Yuriy Bush <yurachi@gmail.com>
2015-10-02 21:26:35 +0100

    completed the ExecutionDateTime validator

--- ServiceSchedulerTests/helpers/Assert.cs
+++ ServiceSchedulerTests/helpers/Assert.cs
@@ -1,5 +1,7 @@
-﻿using ServiceScheduler;
+﻿using NUnit.Framework;
+using ServiceScheduler;
 using System;
+using System.Text;
 
 namespace ServiceSchedulerTests.helpers
 {
@@ -7,8 +9,50 @@ namespace ServiceSchedulerTests.helpers
     {
         public static void ExecutionDateTimeMatch(int expectedHour, int expectedMinute, int expectedDay, int expectedMonth, int expectedYear, bool expectedNow, bool expectedOnce, ExecutionDateTime actual)
         {
-            throw new NotImplementedException();
-            //TODO: throw AssertionException with the mismatched items
+            var sb = new StringBuilder();
+
+            if (expectedHour != actual.ScheduledTime.Hour)
+            {
+                sb.AppendFormat("hour expected:{0} actual:{1}, ", expectedHour, actual.ScheduledTime.Hour);
+            }
+
+            if (expectedMinute != actual.ScheduledTime.Minute)
+            {
+                sb.AppendFormat("minute expected:{0} actual:{1}, ", expectedMinute, actual.ScheduledTime.Minute);
+            }
+
+            if (expectedDay != actual.ScheduledTime.Day)
+            {
+                sb.AppendFormat("day expected:{0} actual:{1}, ", expectedDay, actual.ScheduledTime.Day);
+            }
+
+            if (expectedMonth != actual.ScheduledTime.Month)
+            {
+                sb.AppendFormat("month expected:{0} actual:{1}, ", expectedMonth, actual.ScheduledTime.Month);
+            }
+
+            if (expectedYear != actual.ScheduledTime.Year)
+            {
+                sb.AppendFormat("year expected:{0} actual:{1}, ", expectedYear, actual.ScheduledTime.Year);
+            }
+
+            if (expectedNow != actual.IsNow)
+            {
+                sb.AppendFormat("now expected:{0} actual:{1}, ", expectedNow, actual.IsNow);
+            }
+
+            if (expectedOnce != actual.IsOnce)
+            {
+                sb.AppendFormat("once expected:{0} actual:{1}, ", expectedOnce, actual.IsOnce);
+            }
+
+            if (sb.Length > 0)
+            {
+                sb.Insert(0, "Mismatched ");
+                sb.Replace(',', '.', sb.Length - 2, 1);
+                var message = sb.ToString();
+                throw new AssertionException(message);
+            }
         }
 
     }
